9945e0cc35250150efcc09d205faf59427f69d15
wogks <wogks@wogksui-MacBookPro.local>
2022-09-22 23:24:28 +0900

    Feat: image picker import

---
+++ android/app/src/main/kotlin/com/example/auto_photo_slide/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.auto_photo_slide
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:auto_photo_slide/presentation/main_screen/main_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MainScreen()
+    );
+  }
+}
+
